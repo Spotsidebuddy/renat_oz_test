ddf2208f20473177cb928230759cdecb3f75c232
Spotsidebuddy <spotsidebuddy@gmail.com>
2025-05-23 13:07:29 +0300

    arguably better function

---
+++ get_tallest_super.py
@@ -0,0 +1,77 @@
+import requests
+
+
+def get_tallest_super(gender: str, has_job: bool) -> str:
+    check_data(gender, has_job)
+    all_supers = get_all_supers()
+    tallest_super = get_tallest(all_supers, gender, has_job)
+
+    return tallest_super['name']
+
+
+def check_data(gender: str, has_job: bool) -> None:
+    if gender not in ['Male', 'Female']:
+        print('Gender should be "Male" or "Female".')
+        raise ValueError
+    if not isinstance(has_job, bool):
+        print("Argument 'has_job' should be boolean type")
+        raise TypeError
+
+def get_all_supers() -> list[dict]:
+    url = "https://akabab.github.io/superhero-api/api/all.json"
+    all_supers = requests.get(url)
+    status_code = all_supers.status_code
+    check_connection(status_code)
+    supers_list = all_supers.json()
+    assert_response(supers_list)
+    return supers_list
+
+
+def check_connection(status_code) -> None:
+    if status_code != 200:
+        print("Request could not be completed!")
+        raise ConnectionError
+
+
+def assert_response(data: list[dict]) -> None:
+    if not isinstance(data, list):
+        print("Wrong data recieved. Could not process")
+        raise TypeError
+    if not data:
+        print("Empty response body")
+        raise ValueError
+    if not all(isinstance(item, dict) for item in data):
+        print("Wrong data structure in response body")
+        raise TypeError
+    if not all(item for item in data):
+        print("Empty entries in response.")
+        raise ValueError
+
+def get_tallest(all_supers: list[dict], gender: str, has_job: bool) -> dict:
+    tallest = None
+    top_height = 0
+    for supe in all_supers:
+
+        job = supe['work']['occupation']
+        if job == '-':
+            job = False
+        else:
+            job = True
+
+        gender_curr = supe['appearance']['gender']
+
+        if job == has_job and gender == gender_curr:
+            height_str = supe['appearance']['height'][1]
+            height, units = height_str.split()
+            height = float(height)
+
+            if units == 'meters':
+                height *= 100
+
+            if height > top_height:
+                tallest = supe
+                top_height = height
+
+    return tallest
+
+print(get_tallest_super('Female', True))
---
+++ test_get_tallest_super.py
@@ -0,0 +1,3 @@
+import pytest
+import get_super
+from get_super import get_all_supes
